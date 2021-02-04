e3cb39a76861249d1e99c10d73cae2ba434a5ad9
simple-max <simple.max.899@gmail.com>
2021-02-04 15:38:14 +0100

    added schedule

--- src/cogs/schoolsoft.py
+++ src/cogs/schoolsoft.py
@@ -3,6 +3,7 @@ import requests
 from discord import Embed, Colour
 from discord.ext import commands
 
+
 class SchoolSoft(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,29 +12,89 @@ class SchoolSoft(commands.Cog):
     @commands.command(name="lunch")
     async def lunch(self, ctx):
         """Get the lunch from SchoolSoft"""
-        lunch = requests.get("https://apischoolsoft.herokuapp.com/v1/lunch").json()
-        
+        lunch = requests.get(
+            "https://apischoolsoft.herokuapp.com/v1/lunch").json()
+
         days = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"]
-        embeds = Embed(title="Veckans Lunchmeny",url="https://sms.schoolsoft.se/nti/jsp/student/right_student_lunchmenu.jsp?menu=lunchmenu", colour=Colour(0x46c3db))
-        embeds.set_author(name="SchoolSoft", url="https://sms.schoolsoft.se/nti/jsp/student/right_student_lunchmenu.jsp?menu=lunchmenu", icon_url="https://sms.schoolsoft.se/android-chrome-192x192.png")
+        embeds = Embed(title="Veckans Lunchmeny",
+                       url="https://sms.schoolsoft.se/nti/jsp/student/right_student_lunchmenu.jsp?menu=lunchmenu", colour=Colour(0x46c3db))
+        embeds.set_author(name="SchoolSoft", url="https://sms.schoolsoft.se/nti/jsp/student/right_student_lunchmenu.jsp?menu=lunchmenu",
+                          icon_url="https://sms.schoolsoft.se/android-chrome-192x192.png")
         for day in range(0, 5):
             try:
                 if len(lunch[day]) != 2:
-                    embeds.add_field(name=days[day] ,value=lunch[day][0], inline=False)
+                    embeds.add_field(
+                        name=days[day], value=lunch[day][0], inline=False)
                 else:
-                    embeds.add_field(name=days[day] ,value=f"{lunch[day][0]} \n {lunch[day][1]}", inline=False)
+                    embeds.add_field(
+                        name=days[day], value=f"{lunch[day][0]} \n {lunch[day][1]}", inline=False)
             except Exception as e:
                 print(e)
-        
+
         await ctx.send(embed=embeds)
-        
+
     @commands.command(name="schedule")
-    async def schedule(self, ctx, *, className: str = None, day: str = None):
-        """ Get the schedule from SchoolSoft """
-        print(className)
-        print(day)
-        #Schedule = requests.get(f"https://apischoolsoft.herokuapp.com/v1/schedule/{className}?day=${day}").json()
-        #ToDaysSchedule = requests.get(f"https://apischoolsoft.herokuapp.com/v1/schedule/{className}?today=true").json()
+    async def schedule(self, ctx, inputClassName: str = None, inputDay: str = None):
+        """ Get the schedule from SchoolSoft by class name[required] day[Optional]"""
+        ValidClassName = ["TE18", "TE19", "TE20",
+                          "IT18", "IT19", "IT20", "HA18", "HA19"]
+        SwedishDays = ["måndag", "tisdag", "onsdag", "torsdag", "fredag"]
+        NormalDays = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+        dayIndex = 0
+
+        if inputClassName is None:
+            await ctx.reply("Vänligen ange klassnamnet")
+
+        className = inputClassName.upper()
+
+        if className not in ValidClassName:
+            await ctx.reply("Klassnamnet är ogiltig")
+
+        embed = Embed(
+            url="https://sms.schoolsoft.se/nti/jsp/student/right_student_schedule.jsp?menu=schedule", colour=Colour(0x46c3db))
+        embed.set_author(name="SchoolSoft", url="https://sms.schoolsoft.se/nti/jsp/student/right_student_lunchmenu.jsp?menu=lunchmenu",
+                         icon_url="https://sms.schoolsoft.se/android-chrome-192x192.png")
+
+        if inputDay is None:
+            ToDaysSchedule = requests.get(
+                f"https://apischoolsoft.herokuapp.com/v1/schedule/{className}?today=true").json()
+
+            embed.title = f"Dagens schema"
+            for index in ToDaysSchedule:
+
+                if index['location'] == "None":
+                    embed.add_field(
+                        name=index["subject"], value=f"Klassen börjar kl {index['time']} men klassrum specificerades inte", inline=False)
+                else:
+                    embed.add_field(
+                        name=index["subject"], value=f"Klassen börjar kl {index['time']} i rum {index['location']}", inline=False)
+
+        else:
+
+            if inputDay in SwedishDays:
+                dayIndex = SwedishDays.index(inputDay, 0, 5)
+            elif inputDay in NormalDays:
+                dayIndex = NormalDays.index(inputDay, 0, 5)
+            else:
+                await ctx.reply("Ogiltig dag")
+
+            day = NormalDays[dayIndex]
+
+            Schedule = requests.get(
+                f"https://apischoolsoft.herokuapp.com/v1/schedule/{className}?day={day}").json()
+
+            embed.title = f"{inputDay.capitalize()} schema"
+            for index in Schedule:
+
+                if index['location'] == "None":
+                    embed.add_field(
+                        name=index["subject"], value=f"Klassen börjar kl {index['time']} men klassrum specificerades inte", inline=False)
+                else:
+                    embed.add_field(
+                        name=index["subject"], value=f"Klassen börjar kl {index['time']} i rum {index['location']}", inline=False)
+
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(SchoolSoft(bot))
--- src/main.py
+++ src/main.py
@@ -18,46 +18,48 @@ except Exception as e:
     print("[-] missing module discord")
     print("[-] use -> pip install -r requirements.txt")
     print(f"[-] {e}")
-    
+
+
 class Bot(BotBase):
-        def __init__(self):
-            self.ready = False
-            self.VERSION = None
-            self.command_prefix = str(os.getenv("DEFAULT_PREFIX"))
-            self.description = "NTI Discord Bot Challenge 2021"
-            super().__init__(command_prefix=self.command_prefix, help_command=discord.ext.commands.DefaultHelpCommand(),
-                             description=self.description)
+    def __init__(self):
+        self.ready = False
+        self.VERSION = None
+        self.command_prefix = str(os.getenv("DEFAULT_PREFIX"))
+        self.description = "NTI Discord Bot Challenge 2021"
+        super().__init__(command_prefix=self.command_prefix, help_command=discord.ext.commands.DefaultHelpCommand(),
+                         description=self.description)
 
-        def run(self, version=None):
-            self.VERSION = version
-            print("[?] Bot running ...")
-            return super().run(str(os.getenv("TOKEN")), reconnect=True)
+    def run(self, version=None):
+        self.VERSION = version
+        print("[!] Bot running ...")
+        return super().run(str(os.getenv("TOKEN")), reconnect=True)
 
-        @staticmethod
-        async def on_connect():
-            print("[+] Bot connected and is online")
+    @staticmethod
+    async def on_connect():
+        print("[+] Bot connected and is online")
 
-        @staticmethod
-        async def on_disconnect():
-            print("[-] Bot disconnected")
+    @staticmethod
+    async def on_disconnect():
+        print("[-] Bot disconnected")
 
-        async def on_ready(self):
-            if not self.ready:
-                self.ready = True
-                await self.change_presence(status=discord.Status.online,
-                                           activity=discord.Activity(type=discord.ActivityType.watching,
-                                                                     name=f"Over {len(bot.guilds)} Server"))
-                print("Loading extensions")
-                for filename in os.listdir("./src/cogs"):
-                    if filename.endswith(".py"):
-                        try:
-                            self.load_extension(f"src.cogs.{filename[:-3]}")
-                            print(f"[+] - {filename[:-3]} extension")
-                        except ValueError:
-                            print(f"[-] - {filename[:-3]} extension has issue")
-                            print(f"[-] - {ValueError}")
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            await self.change_presence(status=discord.Status.online,
+                                       activity=discord.Activity(type=discord.ActivityType.watching,
+                                                                 name=f"Over {len(bot.guilds)} Server"))
+            print("[!] Loading extensions")
+            for filename in os.listdir("./src/cogs"):
+                if filename.endswith(".py"):
+                    try:
+                        self.load_extension(f"src.cogs.{filename[:-3]}")
+                        print(f"[+] - {filename[:-3]} extension")
+                    except ValueError:
+                        print(f"[-] - {filename[:-3]} extension has issue")
+                        print(f"[-] - {ValueError}")
 
-            else:
-                print("[!] Bot reconnecting")
+        else:
+            print("[!] Bot reconnecting")
+
 
 bot = Bot()
